d37e692b1a6370e32191e3b899b5ec7284c6b597
Izhar ul Hassan <ezhaar@gmail.com>
2018-02-22 10:13:37 +0100

    replace jupyter notebook with jupyter lab
    Signed-off-by: Izhar ul Hassan <ezhaar@gmail.com>

--- start_notebook.sh
+++ start_notebook.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /root/notebooks
-jupyter notebook --ip=0.0.0.0 --port=9999 --no-browser --allow-root --NotebookApp.token='supersecret'
+jupyter lab --ip=0.0.0.0 --port=9999 --no-browser --allow-root --NotebookApp.token='supersecret'
